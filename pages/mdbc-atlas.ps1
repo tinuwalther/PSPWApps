a5a38bc86ec94183ba270ae90e1795b8a1f264ad
Martin Walther <it@martin-walther.ch>
2022-07-29 09:05:13 +0200

    Add inventory from AltasDB

--- pages/mdbc-atlas.ps1
+++ pages/mdbc-atlas.ps1
@@ -10,7 +10,7 @@ Add-PodeWebPage -Group 'MongoDB' -Name 'MDBC AtlasDB' -Title 'MongoDB Atlas' -Ic
 
         $DatabaseName   = 'tinu'
         $CollectionName = 'power'
-        $mongo_client   = Connect-Mdbc "mongodb+srv://$($WebEvent.Data.Token)@cluster0.epl3x.mongodb.net/?retryWrites=true&w=majority"
+        $mongo_client   = Connect-Mdbc "mongodb+srv://$($WebEvent.Data.Token1)@cluster0.epl3x.mongodb.net/?retryWrites=true&w=majority"
         $mongo_db       = Get-MdbcDatabase -Name $DatabaseName
         $mongo_col      = Get-MdbcCollection -Database $mongo_db -Name $CollectionName
 
@@ -28,8 +28,43 @@ Add-PodeWebPage -Group 'MongoDB' -Name 'MDBC AtlasDB' -Title 'MongoDB Atlas' -Ic
         }
         
     } -Content @(
-        New-PodeWebTextbox -Name 'Token' -Placeholder 'Enter user:token' -Type Password
+        New-PodeWebTextbox -Id 'Token1' -Name 'Token1' -DisplayName 'Token' -Placeholder 'Enter user:token' -Type Password
     )
 
+    New-PodeWebForm -Name "From MongoDB Collection Inventory" -AsCard -ScriptBlock {
+
+        if(-not(Get-InstalledModule -Name Mdbc -ea SilentlyContinue)){
+            Install-Module -Name Mdbc -Force
+            $Error.Clear()
+        }
+        if(-not(Get-Module -Name Mdbc)){ Import-Module -Name Mdbc }
+
+        $DatabaseName   = 'tinu'
+        $CollectionName = 'inventory'
+        $mongo_client   = Connect-Mdbc "mongodb+srv://$($WebEvent.Data.Token2)@cluster0.epl3x.mongodb.net/?retryWrites=true&w=majority"
+        $mongo_db       = Get-MdbcDatabase -Name $DatabaseName
+        $mongo_col      = Get-MdbcCollection -Database $mongo_db -Name $CollectionName
+
+        if($mongo_col){
+            $properties = @(
+                #@{N='ID';E={$($_.ID).ToString()}}
+                @{N='Name';E={$_.DeviceName}}
+                @{N='Type';E={$_.DeviceType}}
+                @{N='CPU';E={$_.'Physical cores'}}
+                @{N='Memory';E={ "$($_.MemoryGB) GB"}}
+                @{N='Diskspace';E={ "$($_.DiskspaceGB) GB"}}
+                @{N='Purchased';E={ (Get-Date $_.PurchaseDate -Format 'yyyy-MM-dd') }}
+                @{N='CHF';E={ "{0:N2}" -f $($_.PriceCHF) }}
+                @{N='Payment';E={ $_.Payment }}
+                @{N='Warranty';E={ $_.Warranty }}
+                @{N='AgeYear';E={ [Math]::Round((New-TimeSpan -Start (Get-Date $_.PurchaseDate -Format 'yyyy-MM-dd') -End (Get-Date -Format 'yyyy-MM-dd') | Select-Object -ExpandProperty Days)/365,1) }}
+            )
+            $data = Get-MdbcData -Collection $mongo_col -As PS | Select-Object $properties
+            $data | Out-PodeWebTable -Sort
+        }
+        
+    } -Content @(
+        New-PodeWebTextbox -Id 'Token2' -Name 'Token2' -DisplayName 'Token' -Placeholder 'Enter user:token' -Type Password
+    )
 
 }
